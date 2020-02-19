5996e420b334ed1fa98980ed2903cc2e5d73c8cb
rypx <46881089+rypx@users.noreply.github.com>
2020-02-20 00:07:26 +0700

    Update webview.h

--- webview.h
+++ webview.h
@@ -1157,9 +1157,9 @@ static int DisplayHTMLPage(struct webview *w) {
 
     char *url = (char *)calloc(1, strlen(webview_url) + 1);
     char *q = url;
-    const char *p;
+    const char *loopThisP = webview_url + strlen(WEBVIEW_DATA_URL_PREFIX);
 
-    for (char *p = webview_url + strlen(WEBVIEW_DATA_URL_PREFIX); *q = *p;
+    for (loopThisP; *q = *p;
          p++, q++) {
       if (*q == '%' && *(p + 1) && *(p + 2)) {
         sscanf(p + 1, "%02x", q);
